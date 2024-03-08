df8ba7bfb3b414a4a223431e8656f31b7d1cc4ae
Heebas101 <130850655+Heebas101@users.noreply.github.com>
2024-03-08 23:34:48 +0000

    Add files via upload

--- SpeechToText/SpeechConversion.py
+++ SpeechToText/SpeechConversion.py
@@ -3,36 +3,34 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 def record_text():
-    #Loop in case of errors
-    while(1):
+    while True:
         try:
-            #use the microphone as source of input. 
             with sr.Microphone() as source2:
-                # Prepare recognizer to receive input
                 r.adjust_for_ambient_noise(source2, duration=0.2)
-
-                #listen for the user's input 
                 audio2 = r.listen(source2)
-
-                #Using google to recognize audio
                 MyText = r.recognize_google(audio2)
 
         except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
+            print("Could not request results: {0}".format(e))
+            continue
 
-        except sr. UnknownValueError:
-            print("unkown value occurrence")
+        except sr.UnknownValueError:
+            print("Unknown value occurrence")
+            continue
+
         return MyText
 
 def output_text(text):
-    f=open("output.txt", "a")
-    f.write(text)
-    f.write("\n")
-    f.close()
-    return
+    try:
+        with open("output.txt", "a") as f:
+            f.write(text + "\n")
+    except IOError as e:
+        print("Error writing to file:", e)
 
-while(1):
-    text = record_text()
-    output_text(text)
-
-    print("Wrote text")
+def live_transcript():
+    while True:
+        print("Listening...")
+        text = record_text()
+        print("Recognized:", text)
+        output_text(text)
+        print("Transcription saved to output.txt")
